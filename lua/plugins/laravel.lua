44668fcceae9af926a3d80bdd8eeebd9152722ee
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-11-07 14:48:45 +0700

    feat: delete laravel key binding

--- lua/plugins/laravel.lua
+++ lua/plugins/laravel.lua
@@ -7,11 +7,11 @@ return {
         "kevinhwang91/promise-async",
     },
     cmd = { "Laravel" },
-    keys = {
-        { "<leader>la", ":Laravel artisan<cr>" },
-        { "<leader>lr", ":Laravel routes<cr>" },
-        { "<leader>lm", ":Laravel related<cr>" },
-    },
+    -- keys = {
+    --     { "<leader>la", ":Laravel artisan<cr>" },
+    --     { "<leader>lr", ":Laravel routes<cr>" },
+    --     { "<leader>lm", ":Laravel related<cr>" },
+    -- },
     event = { "VeryLazy" },
     opts = {},
     config = true,
