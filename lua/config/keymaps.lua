29c55cdf835f572cc8ce007af417211ad89fe12c
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-11-10 08:57:45 +0700

    feat: map c-d to c-dzz

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- add zz after ctrl d and ctrl u
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
